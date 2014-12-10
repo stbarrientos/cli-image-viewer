72c4401d7a530fe6503638720a0d7cdb89df5fb3
stbarrientos <stbarrientos@gmail.com>
2014-12-10 14:14:30 -0500

    set up node server

--- image-converter.js
+++ image-converter.js
@@ -1,15 +1,26 @@
 #!/usr/bin/env node
 
-//var program = require("commander")
-
-//program
-//  .version("0.0.1")
-//  .option("-u, --url", "Go To Url")
-//  .parse(process.argv)
+var path = require("path");
+var express = require("express");
+var pkg = require( path.join(__dirname, "package.json"));
+var target;
 
 if (process.argv[2]){
-  console.log("I will go there. " + process.argv[2]);
+   target = process.argv[2];
 } else {
   console.log("\n\nUsage: cli-image-viewer [url]\n\nExample: cli-image-viewer http://www.survivingcollege.com/wp-content/uploads/2014/10/grumpycat.jpg\n\n");
+  target = "No target specified";
 }
 
+var port = 3000;
+var app = express();
+
+app.use("/", express.static(path.join(__dirname, 'frontend')));
+
+app.get("/image", function(req,res) {
+  res.send(target);
+  console.log("target: " + target);
+});
+
+app.listen(port);
+console.log("Listening on port " + port);
